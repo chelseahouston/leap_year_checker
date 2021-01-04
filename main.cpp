1639d0112536ffb146a7f7ac77fe5a2cc529546a
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-04 09:54:08 +0000

    formatting

--- main.cpp
+++ main.cpp
@@ -8,17 +8,17 @@ int main() {
     cin >> year;
 
     if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0)
-                cout << year << " is/was a leap year.";
-            else
-                cout << year << " is/was not a leap year.";
+      if (year % 100 == 0) {
+        if (year % 400 == 0)
+          cout << year << " is/was a leap year.";
+        else
+          cout << year << " is/was not a leap year.";
         }
         else
-            cout << year << " is/was a leap year.";
+          cout << year << " is/was a leap year.";
     }
     else
-        cout << year << " is/was not a leap year.";
+      cout << year << " is/was not a leap year.";
 
     return 0;
 }
